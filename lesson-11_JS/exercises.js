4164b8a1e450d450c0d39820fc555332c400e183
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2024-06-17 17:16:51 +0500

    func theme

--- lesson-11_JS/exercises.js
+++ lesson-11_JS/exercises.js
@@ -1,19 +1,19 @@
 // Напишите функцию, которая принимает массив чисел и находит 
 // второе наименьшее и второе наибольшее числа.      
 
-function findFormerMaxMinNums(arr) {
-    let new_sorted_arr = arr.sort((a, b) => a - b)
-    return [new_sorted_arr[1],  new_sorted_arr[new_sorted_arr.length-2]]
-}
+// function findFormerMaxMinNums(arr) {
+//     let new_sorted_arr = arr.sort((a, b) => a - b)
+//     return [new_sorted_arr[1],  new_sorted_arr[new_sorted_arr.length-2]]
+// }
 
-let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let r = findFormerMaxMinNums(list) 
-console.log(r) // [9, 2]
+// let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let r = findFormerMaxMinNums(list) 
+// console.log(r) // [9, 2]
 
 
-let list2 = [100, 2, 1, 10, 124]
-let r2 = findFormerMaxMinNums(list2)
-console.log(r2) // [2, 100]
+// let list2 = [100, 2, 1, 10, 124]
+// let r2 = findFormerMaxMinNums(list2)
+// console.log(r2) // [2, 100]
 
 
 
