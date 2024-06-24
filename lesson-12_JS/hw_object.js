45f9ec7e978c7ac65afba07bf3090ae2574167aa
Alisher <alisherxujanov163@gmail.com>
2024-06-24 22:36:10 +0500

    Co-authored-by: Шохрух <shokhrukh7oo7@users.noreply.github.com>

--- lesson-12_JS/hw_object.js
+++ lesson-12_JS/hw_object.js
@@ -44,37 +44,97 @@
 // console.log("new obj: ", newObject);
 // console.log("Deleted key: ", z);
 
-// ================ INTERMEDIATE LEVEL ==============
+// ================== INTERMEDIATE LEVEL (Junior) ===================
 // 1.Создайте функцию, которая принимает объект
 //  в качестве параметра и возвращает сумму всех значений,
 //  которые являются числами.
 
-// const number = {
+// const NUMBERS = {
 //   a: 4,
 //   b: 7,
+//   g: 'Hello',
 //   c: 10,
 //   d: 23,
 //   e: 77,
 // };
-// const sum = addSum(number);
+// const sum = addSum(NUMBERS);
 
 // function addSum(obj) {
+//   // acc == 0,   next == 4        =>  4
+//   // acc == 4,   next == 7        =>  11
+//   // acc == 11,  next == "Hello"  =>  11
+//   // ...
 //   return Object.values(obj).reduce((acc, next) => {
-//     if (typeof Object.values(obj) === "number") return acc + next;
-//   });
+//     if (typeof(next) === "number") return acc + next
+//     return acc
+//   }, 0);
 // }
-// console.log(sum);
+// console.log(sum)
 
 // ============================================================================================
-// 2.Создайте функцию, которая принимает объект
-//  в качестве параметра и преобразует каждый ключ в обратный
-//  регистр (назад-вперёд) и возвращает новый объект.
+// 2. Создайте функцию, которая принимает объект в качестве параметра и
+//    меняет местами ключи с значениями.
+
+// let testObject = {
+//   a: "Alisher",
+//   b: "Shokhrukh",
+//   c: "Olim",
+//   d: undefined,
+//   e: 99,
+// }
+// OUTPUT:
+// {
+//   "Alisher" : "A",
+//   "Shokhrukh": "B",
+//   "Olim": "C",
+//   d: undefined,
+//   e: 99,
+// }
+// ------------------------------
+// function swapKeyValues(obj) {
+//   const result = {}
+//   Object.keys(obj).forEach(key => {
+//     const val = obj[key]
+//     if (typeof(val) === 'string') {
+//       result[val] = key
+//     } else {
+//       result[key] = val
+//     }
+//   })
+//   return result
+// }
+// let result1 = swapKeyValues(testObject)
+// console.log(result1)
 
 // ============================================================================================
 
 // 3.Создайте функцию, которая принимает столько объектов,
 //  сколько захотите, и возвращает объект,
 //  который содержит все ключи и значения переданных объектов.
+
+// let obj1 = { name: "Alisher", age: 25 }
+// let obj2 = { hobby: "Learn programming", isMerried: false }
+// let obj3 = { city: "Tashkent", country: "Uzbekistan" }
+// let obj4 = { job: "Frontend developer", salary: 1000 }
+// let obj5 = { isStudent: true, isEmployed: false }
+
+
+// function mergeObjects(...objects) {
+//   const RESULT = {}
+//   objects.forEach(obj => {
+//     Object.keys(obj).forEach(key => {
+//       RESULT[key] = obj[key]
+//     })
+//   })
+//   return RESULT
+//   // ----------------------------------------
+//   // return objects.reduce((acc, next) => {
+//   //   return {...acc, ...next}
+//   // })
+// }
+
+// let result2 = mergeObjects(obj1, obj2, obj3, obj4, obj5)
+// console.table(result2)
 
 // ============================================================================================
 
@@ -92,30 +152,78 @@
 // const newKey = keySort(defObj);
 
 // function keySort(obj) {
-//   const sortKeys = Object.keys(obj).sort();
-//   const sortObj = {};
+//   const sortKeys = Object.keys(obj).sort()
+//   const sortObj = {}
 
 //   sortKeys.forEach((e) => {
-//     sortObj[e] = obj[e];
-//   });
-//   return sortObj;
+//     sortObj[e] = obj[e]
+//   })
+//   return sortObj
 // }
-// console.log(newKey);
 
+// console.log(newKey)
 // ============================================================================================
 
 // 5.Создайте функцию, которая принимает объект в
 // качестве параметра и возвращает объект, отсортированный
-//  по его значениям (а не ключам)
+// по его значениям (а не ключам)
 
-let userInfo = {
-  name: "вася",
-  age: 30,
-  "like js": true,
-};
 
-let user = Object.assign({}, userInfo);
+// function valueSort(obj) {
+//   const valSort = Object.values(obj).sort((a, b) => a - b);
+//   const result = {};
 
-user.age = 20;
-console.log("default user info :", userInfo);
-console.log("new user info :", user);
+//   valSort.forEach(num => {
+//     // 1. You have a number
+//     // 2. You have to find that number's location so that you can find the key
+//     Object.keys(obj).forEach(key => {
+//       if(obj[key] === num){
+//         result[key] = num;
+//       }
+//     })
+//   })
+//   return result;
+// }
+
+
+// let testObject = { a: 15, b: 10, c: 20, d: 5 }
+// // [5, 10, 15, 20]
+// // OUTPUT:
+// // { d: 5,   b: 10,   a: 15,   c: 20 }
+// let result3 = valueSort(testObject)
+// console.table(result3)
+
+
+// ================== ADVANCED LEVEL (Pro) ===================
+// Напишите функцию, чтобы подсчитать, 
+// сколько раз каждое значение встречается в объекте. 
+
+
+function countValues(obj) {
+  const RESULT = {}
+
+  let vals = Object.values(obj)
+  vals.forEach(val => {
+    const KEY = String(val)
+    if(Object.keys(RESULT).includes(KEY)) {
+      RESULT[KEY] += 1
+    } else {
+      RESULT[KEY] = 1
+    }
+  })
+  return RESULT
+}
+
+
+
+let testObject = {
+  a: 5, b: 10, c: 15,
+  d: 5, e: 10, f: 15,
+  g: 20, h: true, i: 20,
+  j: 20, k: true, l: 35,
+  m:undefined,  n:null,  o:undefined
+}
+// OUTPUT:
+// { "5":2,  "10":2,  "15":2,  ... ...}
+let result4 = countValues(testObject)
+console.table(result4)
